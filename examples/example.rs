043989e1db5daf1e6be5cf3ce6c053965e1bf6f9
Jakub Koralewski <contact@jcubed.me>
2020-02-19 12:12:27 +0100

    Specify example in Cargo.toml

--- examples/example.rs
+++ examples/example.rs
@@ -1,17 +1,19 @@
 use prproj::PremiereReader;
 use std::path::Path;
+use std::time::Instant;
 
-#[allow(dead_code)]
 const XML_FILE: &str = r"examples\test.unzipped.prproj";
 //const GZ_FILE: &str = r"examples\test.zipped.prproj";
 
 fn main() {
+    let before = Instant::now();
     let mut reader = PremiereReader::from_path(&Path::new(XML_FILE));
 	if let Err(err) = reader.read() {
         println!("Error reading Premiere Pro file: {}\n{:#?}!", err, err)
     }
-    println!("Sequences: {:#?}", reader.sequences());
-    println!("Media: {:#?}", reader.media())
+    println!("Time to read: {:#?}", Instant::now() - before);
+//    println!("Sequences: {:#?}", reader.sequences());
+//    println!("Media: {:#?}", reader.media());
 }
 
 
